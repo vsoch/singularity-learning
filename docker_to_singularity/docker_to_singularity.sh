1d36433ad1e938af9b416ec223452cbfdf3c911d
vsoch <vsochat@stanford.edu>
2016-06-10 13:06:37 -0700

    reorganizing into folders

--- docker_to_singularity/docker_to_singularity.sh
+++ docker_to_singularity/docker_to_singularity.sh
@@ -1,3 +1,7 @@
 # from @oesteban
 sudo singularity create /tmp/container.img
 docker export [container name] | sudo singularity import /tmp/container.img
+
+# Try with jshint
+sudo singularity create jshint.img
+docker export 1d9cef51faba | sudo singularity import jshint.img
